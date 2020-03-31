329f2d53f74bf31014c0141dba5485a3259b952d
Virjanand <virjanand@virjanand.com>
2020-03-31 06:33:48 +0200

    Added routes for screens

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,31 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
         ),
       ),
       home: WelcomeScreen(),
+      routes: {
+        'welcome': (context) => WelcomeScreen(),
+        'login': (context) => LoginScreen(),
+        'registration': (context) => RegistrationScreen(),
+        'chat': (context) => ChatScreen(),
+      },
+      initialRoute: 'login',
     );
   }
 }
