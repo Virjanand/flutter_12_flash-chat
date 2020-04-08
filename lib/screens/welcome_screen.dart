ee11e42523472e5159250c084d0accc409689576
Virjanand <virjanand@virjanand.com>
2020-04-08 05:47:21 +0200

    Loop animation for logo

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -27,10 +27,24 @@ class _WelcomeScreenState extends State<WelcomeScreen>
     animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
     controller.forward();
 
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse(from: 1.0);
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+
     controller.addListener(() {
       setState(() {});
       print(animation.value);
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
